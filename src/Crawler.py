e0da14288058ed640b054a5c821df2b6c101a9c1
andres-lou <lourios.andres@gmail.com>
2023-08-04 22:37:26 +0200

    transferred functionality if saving and loading to `Crawler` class and moved imports into `util`

--- src/Crawler.py
+++ src/Crawler.py
@@ -6,6 +6,47 @@ from src.util import *
 class Crawler:
     def __init__(self, site_map: SiteMap):
         self.site_map = site_map
+        self.parallel_documents: List[ParallelDocument] = []
+
+    def save_parallel_documents_to_disk(self, loc: str = None) -> None:
+        d = {}
+        for parallel_doc in self.parallel_documents:
+            d[parallel_doc.url] = {"langs": parallel_doc.langs, "main_lang": parallel_doc.main_lang}
+        if loc is None:
+            loc = "parallel_docs.json"
+        with open(loc, "w") as f:
+            f.write(json.dumps(d))
+        logging.info(f"Saved {len(self.parallel_documents)} parallel documents to disk")
+
+    def load_parallel_documents_from_disk(self, loc: str) -> None:
+        with open(loc) as f:
+            parallel_docs = json.loads(f.read())
+        self.parallel_documents = [
+            ParallelDocument(
+                url=key,
+                langs=parallel_docs[key]["langs"],
+                main_lang=parallel_docs[key]["main_lang"]
+            ) for key in parallel_docs.keys()
+        ]
+        logging.info(f"Loaded {len(self.parallel_documents)} parallel documents from disk")
+
+    def save_visited_urls_to_disk(self, loc: str = None) -> None:
+        if loc is None:
+            loc = "visited_urls.json"
+        with open(loc, "w") as f:
+            f.write(json.dumps(self.site_map.visited_urls))
+        logging.info(
+            f"Saved {len([key for key in self.site_map.visited_urls.keys() if self.site_map.visited_urls[key]])} "
+            f"visited urls to disk"
+        )
+
+    def load_visited_urls_from_disk(self, loc: str) -> None:
+        with open(loc) as f:
+            self.site_map.visited_urls = json.loads(f.read())
+        logging.info(
+            f"Loaded {len([key for key in self.site_map.visited_urls.keys() if self.site_map.visited_urls[key]])} "
+            f"visited urls from file"
+        )
 
     def gather_parallel_documents(self, driver: webdriver,
                                   save_interval: int = 50,
@@ -13,12 +54,13 @@ class Crawler:
                                   parallel_documents_file: str = None):
 
         if visited_urls_file is not None:
-            self.site_map.load_visited_urls_from_disk(visited_urls_file)
+            self.load_visited_urls_from_disk(visited_urls_file)
 
         if parallel_documents_file is not None:
-            self.site_map.load_parallel_documents_from_disk(parallel_documents_file)
+            self.load_parallel_documents_from_disk(parallel_documents_file)
 
-        for idx, url in enumerate(key for key in self.site_map.visited_urls.keys() if self.site_map.visited_urls[key] is False):
+        for idx, url in enumerate(key for key in self.site_map.visited_urls.keys() if
+                                  self.site_map.visited_urls[key] is False):
             driver.get(url)
             langs = []
             for language in LANGS:
@@ -34,10 +76,9 @@ class Crawler:
                         logging.debug(f"'{language}' not found in document")
                 except NoSuchElementException:
                     logging.debug(f"No parallel document at {url}")
-            if len(langs) != 0 or langs != ['es']:
-                self.site_map.parallel_documents.append(
+            if langs != [self.site_map.main_language]:
+                self.parallel_documents.append(
                     ParallelDocument(
-
                         url=url,
                         langs=langs
                     )
@@ -47,8 +88,8 @@ class Crawler:
                 logging.debug(f"Parallel document at {url} does not contain Mayan languages")
             self.site_map.visited_urls[url] = True
             if idx % save_interval == 0 and idx != 0:
-                self.site_map.save_parallel_documents_to_disk()
-                self.site_map.save_visited_urls_to_disk()
+                self.save_parallel_documents_to_disk()
+                self.save_visited_urls_to_disk()
 
-        self.site_map.save_visited_urls_to_disk()
-        self.site_map.save_parallel_documents_to_disk()
+        self.save_visited_urls_to_disk()
+        self.save_parallel_documents_to_disk()
--- src/SiteMap.py
+++ src/SiteMap.py
@@ -1,20 +1,15 @@
-import json
-import requests
-from src.ParallelDocument import ParallelDocument
 from src.util import *
-from lxml import etree
-from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox import webdriver
 
 
 class SiteMap:
 
     def __init__(self,
                  url: str,
+                 main_language: str = "es",
                  exclude: List[str] = None):
         self.map_url = url
+        self.main_language = main_language
         self.visited_urls = {}
-        self.parallel_documents = []
         response = requests.get(self.map_url)
         root = etree.fromstring(response.content)
         for sitemap in root:
@@ -24,37 +19,3 @@ class SiteMap:
             for ex in exclude:
                 self.visited_urls = {url: False for url in self.visited_urls.keys() if ex not in url}
         logging.info(f"Collected {len(self.visited_urls)} from site map")
-
-    def save_parallel_documents_to_disk(self, loc: str = None) -> None:
-        d = {}
-        for parallel_doc in self.parallel_documents:
-            d[parallel_doc.url] = {"langs": parallel_doc.langs, "main_lang": parallel_doc.main_lang}
-        if loc is None:
-            loc = "parallel_docs.json"
-        with open(loc, "w") as f:
-            f.write(json.dumps(d))
-        logging.info(f"Saved {len(self.parallel_documents)} parallel documents to disk")
-
-    def load_parallel_documents_from_disk(self, loc: str) -> None:
-        with open(loc) as f:
-            parallel_docs = json.loads(f.read())
-        self.parallel_documents = [
-            ParallelDocument(
-                url=key,
-                langs=parallel_docs[key]["langs"],
-                main_lang=parallel_docs[key]["main_lang"]
-            ) for key in parallel_docs.keys()
-        ]
-        logging.info(f"Loaded {len(self.parallel_documents)} parallel documents from disk")
-
-    def save_visited_urls_to_disk(self, loc: str = None) -> None:
-        if loc is None:
-            loc = "visited_urls.json"
-        with open(loc, "w") as f:
-            f.write(json.dumps(self.visited_urls))
-        logging.info(f"Saved {len([key for key in self.visited_urls.keys() if self.visited_urls[key]])} visited urls to disk")
-
-    def load_visited_urls_from_disk(self, loc: str) -> None:
-        with open(loc) as f:
-            self.visited_urls = json.loads(f.read())
-        logging.info(f"Loaded {len([key for key in self.visited_urls.keys() if self.visited_urls[key]])} visited urls from file")
