3b5a0172c593c50378b1801166a75d146515f2c3
andres-lou <lourios.andres@gmail.com>
2023-08-04 15:02:37 +0200

    moved crawling functionality to `Crawler` class

--- src/SiteMap.py
+++ src/SiteMap.py
@@ -13,18 +13,18 @@ class SiteMap:
                  url: str,
                  exclude: List[str] = None):
         self.map_url = url
-        self.urls = {}
+        self.visited_urls = {}
         self.parallel_documents = []
         response = requests.get(self.map_url)
         root = etree.fromstring(response.content)
         for sitemap in root:
             children = sitemap.getchildren()
-            self.urls[children[0].text] = False
+            self.visited_urls[children[0].text] = False
         if exclude is not None:
             for ex in exclude:
-                self.urls = {url: False for url in self.urls.keys() if ex not in url}
+                self.visited_urls = {url: False for url in self.visited_urls.keys() if ex not in url}
+        logging.info(f"Collected {len(self.visited_urls)} from site map")
 
-    @staticmethod
     def save_parallel_documents_to_disk(self, loc: str = None) -> None:
         d = {}
         for parallel_doc in self.parallel_documents:
@@ -33,52 +33,28 @@ class SiteMap:
             loc = "parallel_docs.json"
         with open(loc, "w") as f:
             f.write(json.dumps(d))
+        logging.info(f"Saved {len(self.parallel_documents)} parallel documents to disk")
 
-    @staticmethod
     def load_parallel_documents_from_disk(self, loc: str) -> None:
         with open(loc) as f:
             parallel_docs = json.loads(f.read())
-        self.parallel_documents = [ParallelDocument(
-            url=key,
-            langs=parallel_docs[key]["langs"],
-            main_lang=parallel_docs[key]["main_lang"]
-        ) for key in parallel_docs.keys()]
+        self.parallel_documents = [
+            ParallelDocument(
+                url=key,
+                langs=parallel_docs[key]["langs"],
+                main_lang=parallel_docs[key]["main_lang"]
+            ) for key in parallel_docs.keys()
+        ]
+        logging.info(f"Loaded {len(self.parallel_documents)} parallel documents from disk")
 
-    @staticmethod
     def save_visited_urls_to_disk(self, loc: str = None) -> None:
         if loc is None:
-            loc = "crawled_urls.json"
+            loc = "visited_urls.json"
         with open(loc, "w") as f:
-            f.write(json.dumps(self.urls))
+            f.write(json.dumps(self.visited_urls))
+        logging.info(f"Saved {len([key for key in self.visited_urls.keys() if self.visited_urls[key]])} visited urls to disk")
 
-    @staticmethod
     def load_visited_urls_from_disk(self, loc: str) -> None:
         with open(loc) as f:
-            self.urls = json.loads(f.read())
-
-    def get_parallel_documents(self, driver: webdriver):
-        for url in self.urls.keys():
-            driver.get(url)
-            langs = []
-            for language in LANGS:
-                try:
-                    language_input = driver.find_element(By.XPATH, ".//input[@id='otherAvailLangsChooser']")
-                    language_input.clear()
-                    language_input.send_keys(language)
-                    sleep(1)
-                    try:
-                        driver.find_element(By.XPATH, f".//li[@data-value='{language}']")
-                        langs.append(language)
-                    except NoSuchElementException:
-                        logging.debug(f"'{language}' not found in document")
-                except NoSuchElementException:
-                    logging.info(f"No parallel document at {url}")
-            if len(langs) != 0:
-                self.parallel_documents.append(ParallelDocument(
-                    url=url,
-                    langs=langs
-                ))
-            else:
-                logging.debug(f"Parallel document at {url} does not contain Mayan languages")
-            self.urls[url] = True
-
+            self.visited_urls = json.loads(f.read())
+        logging.info(f"Loaded {len([key for key in self.visited_urls.keys() if self.visited_urls[key]])} visited urls from file")
