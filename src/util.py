d69137f871cf37706ae229e3ecc5aa2df27c2221
andres-lou <lourios.andres@gmail.com>
2023-08-05 20:06:35 +0200

    added modules

--- src/util.py
+++ src/util.py
@@ -1,12 +1,18 @@
 import logging
 import sys
+import json
+import requests
+import os
 from time import sleep
 from typing import List, Dict
 import pandas as pd
+from lxml import etree
 from selenium.common import NoSuchElementException
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox import webdriver
+from selenium import webdriver
+
 from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 
